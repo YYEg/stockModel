c85fe8ad3258bd01c94931e40c09d50a70285429
Egor <yyrkovskii@mail.ru>
2023-10-19 00:30:23 +0800

    some unespected errors with inverse_transform!!!

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 # library's
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import pandas_datareader as web
-import datetime as dt
-import tensorflow as tf
+from datetime import datetime
+import yfinance as yf
 
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 
 # Load Data
-company = 'FB'
-start = dt.datetime(2012, 1, 1)
-end = dt.datetime(2020, 1, 1)
+company = 'TATAPOWER'
+yf.pdr_override()
+y_simbols = ['TATAPOWER.NS']
+start = datetime(2012, 1, 1)
+end = datetime(2020, 1, 1)
 
-data = web.DataReader(company, 'yahoo', start, end)
+data = yf.download(y_simbols, start=start, end=end)
 
 # Prepare Data
 scaler = MinMaxScaler(feature_range=(0, 1))
@@ -46,4 +49,37 @@ model.add(Dense(units=1))  # Prediction on the next closing day
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
+''' Test The Model Accuracy on Existing Data'''
+test_start = datetime(2020, 1, 1)
+test_end = datetime.now()
 
+test_data = yf.download(y_simbols, start=test_start, end=test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - predict_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scaler.transform(model_inputs)
+
+# Make Predictions on Test Data
+
+x_test = []
+
+for x in range(predict_days, len(model_inputs)):
+    x_test.append(model_inputs[x-predict_days:x, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+
+# Plot The Test Predictions
+plt.plot(actual_prices, color='black', label=f"Actual {company} price")
+plt.plot(predicted_prices, color='green', label=f"Predict {company} price")
+plt.title(f"{company} Share price")
+plt.xlabel('Time')
+plt.ylabel(f"{company} Share price")
+plt.legend()
+plt.show()
